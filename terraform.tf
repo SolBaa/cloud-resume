4278dadc8754b299c1da2b4f9024805d1bcc5248
Sol Battaglia <solbattaglia@gmail.com>
2023-06-05 16:25:45 -0300

    first commit

---
+++ terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_s3_bucket" "resume" {
+  bucket = "sol-test-152"
+}
+
+# Upload an object
+resource "aws_s3_bucket_object" "object1" {
+for_each = fileset("./resume/", "**")
+bucket = data.aws_s3_bucket.resume.id
+key = each.value
+source = "./resume/${each.value}"
+etag = filemd5("./resume/${each.value}")
+}
